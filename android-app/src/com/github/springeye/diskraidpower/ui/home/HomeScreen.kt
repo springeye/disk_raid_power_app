15a7f8f3f3cc4a0458f2564fc9c2018f3acfed84
chenjin <henjue@gmail.cm>
2025-10-10 16:53:22 +0800

    update

--- android-app/src/com/github/springeye/diskraidpower/ui/home/HomeScreen.kt
+++ android-app/src/com/github/springeye/diskraidpower/ui/home/HomeScreen.kt
@@ -1,14 +1,28 @@
 package com.github.springeye.diskraidpower.ui.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     state: HomeState,
     onAction: (HomeAction) -> Unit
 ) {
-    // TODO UI Rendering
+    Scaffold(topBar = {
+        TopAppBar(title = {Text("111")})
+    }){
+        Column(modifier= Modifier.padding(it)) {
+            Text("Home page")
+        }
+    }
 }
 
 @Composable
